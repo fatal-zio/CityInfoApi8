3348eebb249dea0e5b84e23be0a9a25c0906e8bd
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-14 16:15:58 -0600

    Implemented in memory datastore

---
+++ CityInfo.Api/CitiesDataStore.cs
@@ -0,0 +1,35 @@
+﻿using CityInfo.Api.Models;
+
+namespace CityInfo.Api
+{
+    public class CitiesDataStore
+    {
+        public CitiesDataStore()
+        {
+            Cities =
+            [
+                new()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park."
+                },
+                new()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished."
+                },
+                new()
+                {
+                    Id=3,
+                    Name = "Paris",
+                    Description = "The one with the really big tower."
+                }
+            ];
+        }
+
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+    }
+}
