99398ede062629e5d7b4a829f485e7b5ca78bf46
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-14 17:07:09 -0600

    Added Points of Interest.

--- CityInfo.Api/CitiesDataStore.cs
+++ CityInfo.Api/CitiesDataStore.cs
@@ -12,19 +12,54 @@ namespace CityInfo.Api
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with the big park."
+                    Description = "The one with the big park.",
+                    PointsOfInterest =
+                    [
+                        new() {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the united states."
+                        },
+                        new() {
+                            Id = 1,
+                            Name = "Empire State Building",
+                            Description = "A 102 story skyscraper located in Midtown Manhattan."
+                        }
+                    ]
                 },
                 new()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "The one with the cathedral that was never really finished."
+                    Description = "The one with the cathedral that was never really finished.",
+                    PointsOfInterest =
+                    [
+                        new() {
+                            Id = 3,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans." },
+                        new() {
+                            Id = 4,
+                            Name = "Antwerp Central Station",
+                            Description = "The the finest example of railway architecture in Belgium." },
+                    ]
                 },
                 new()
                 {
                     Id=3,
                     Name = "Paris",
-                    Description = "The one with the really big tower."
+                    Description = "The one with the really big tower.",
+                    PointsOfInterest =
+                    [
+                        new() {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel." },
+                        new() {
+                            Id = 6,
+                            Name = "The Louvre",
+                            Description = "The world's largest museum." },
+                    ]
                 }
             ];
         }
