300d50e3d11557ae3039cee0ad1c97c9808c527e
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-28 18:32:43 -0600

    Added DbContext

---
+++ CityInfo.Api/DbContexts/CityInfoContext.cs
@@ -0,0 +1,18 @@
+﻿using CityInfo.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.Api.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -1,6 +1,8 @@
 using CityInfo.Api;
+using CityInfo.Api.DbContexts;
 using CityInfo.Api.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -44,6 +46,9 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 
 builder.Services.AddSingleton<CitiesDataStore>();
+
+builder.Services.AddDbContext<CityInfoContext>(dbContext =>
+    dbContext.UseSqlite("DataSource=CityInfo.db"));
 
 var app = builder.Build();
 
