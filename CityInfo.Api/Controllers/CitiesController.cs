c2615e305be4ef0932ce9168472f578fe0daf673
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-31 17:53:55 -0600

    Added searchQuery to Cities

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -16,9 +16,9 @@ namespace CityInfo.Api.Controllers
         private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(string? name)
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(string? name, string? searchQuery)
         {
-            var cityEntities = await _cityInfoRepository.GetCitiesAsync(name);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync(name, searchQuery);
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
         }
 
