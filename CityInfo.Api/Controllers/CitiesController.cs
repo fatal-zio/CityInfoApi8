e1d1a3b43a89d9bd836d7f6874b7326f260bb8fd
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-31 17:40:37 -0600

    Added Name Query Parameter on Cities

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -12,12 +12,13 @@ namespace CityInfo.Api.Controllers
 
         private readonly ICityInfoRepository _cityInfoRepository =
             cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
+
         private readonly IMapper _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(string? name)
         {
-            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync(name);
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
         }
 
