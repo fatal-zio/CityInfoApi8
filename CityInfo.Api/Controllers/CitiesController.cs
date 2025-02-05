ea9ab4a4b3a91aec7d3daa414879ff96972a9686
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-02-05 17:16:28 -0600

    Added return code documentation example

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -43,7 +43,11 @@ namespace CityInfo.Api.Controllers
         /// <param name="id">The Id of the City to Get</param>
         /// <param name="includePointsOfInterest">Whether or not to include Points of Interest</param>
         /// <returns>A city with or without points of interest</returns>
+        /// <response code="200">Returns the requested city</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
         {
             var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
